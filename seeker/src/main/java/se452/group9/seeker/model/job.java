4548798cf451ff1307cf636e8028e9bce48eef36
Korey Lo <koreylo9@gmail.com>
2021-04-19 20:47:41 -0700

    added job and jobLocation classes

---
+++ seeker/src/main/java/se452/group9/seeker/model/job.java
@@ -0,0 +1,16 @@
+package se452.group9.seeker.model;
+
+import java.sql.Date;
+
+public class job {
+
+    private int jobID;
+    private string title;
+    private string description;
+    // private string jobType;
+    private Date createdDate;
+    private Boolean isActive;
+    // private int postedByID;
+    // private int companyID;
+    // private jobSkills
+}
---
+++ seeker/src/main/java/se452/group9/seeker/model/jobLocation.java
@@ -0,0 +1,12 @@
+package se452.group9.seeker.model;
+
+import java.sql.Date;
+
+public class jobLocation {
+
+    // private string jobType;
+    // private int postedByID;
+    // private int companyID;
+    // private int jobLocationID;
+    // private jobSkills
+}
